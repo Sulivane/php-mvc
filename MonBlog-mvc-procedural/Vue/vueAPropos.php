a83b08c441160969a3344a875558b63ec51034a2
Sulivane <steven1erpas@gmail.com>
2020-09-21 14:27:14 +0200

    add view APropos

---
+++ MonBlog-mvc-procedural/Vue/vueAPropos.php
@@ -0,0 +1,5 @@
+<?php $titre = 'Mon Blog' ?>
+
+<?php ob_start(); ?>
+<p>Blog réalisé par M.Batiste Pesquet.Un grand merci a lui pour ses ressources pédagique</p>
+<?php $contenu = ob_get_clean(); ?>
